8657c9bb6391d1c55338ab38bac0979f0f7ec347
vinusham10 <141708050+vinusham10@users.noreply.github.com>
2023-08-21 12:36:49 +0530

    app.js

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
-let button= document.getElementById("button");
+let button = document.getElementById("button");
 var numberInTheShape = 1;
 var oldValue = 0;
-
+ 
 button.onclick = () => {
     let n = document.getElementById("number").value;
     const circle = document.getElementById("circle");
     const square = document.getElementById("square");
-    const rect = document.getElementById("rect");
-    const $box = document.getElementById("$box");
+    const rect = document.getElementById("rectangle");
+    const $box = document.getElementById("box");
     let j;
 
-    n = Number(oldValue)+ Number(n);
-
-    for(let j=numberInTheShape; j<=n; j++)
-    {
+    n = Number(oldValue) + Number(n);
+    for(let j = numberInTheShape;j<=n;j++){
         var shape = document.createElement("div");
-        shape.innerHTML+= numberInTheShape;
-        if(square.checked)
-        {
-            shape.classList.add("square")
+        shape.innerHTML += numberInTheShape;
+        if(square.checked){
+            shape.classList.add("square");
         }
-        else if(circle.checked)
-        {
-            shape.classList.add("circle")
+        else if(circle.checked){
+            shape.classList.add("circle");
         }
-        else if(rect.checked)
-        {
-            shape.classList.add("rectangle")
+        else if(rect.checked){
+            shape.classList.add("rectangle");
         }
         else{
-            document.write("Invalid input")
+            document.write("Invalid Input");
         }
-    $box.appendChild(shape)
-    numberInTheShape++;
-    oldValue= document.getElementById("box").lastElementChild.innerHTML;
-    }
-};
+        $box.appendChild(shape);
+        numberInTheShape++;
+        oldValue = document.getElementById("box").lastElementChild.innerHTML;
+    }
+}
